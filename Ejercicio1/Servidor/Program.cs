c5c20732747955f337ad89724f84cc7e7ffe0c07
liabafi <lbatllefigueras@gmail.com>
2025-04-21 12:21:59 +0200

    Crear conexión servidor TCP

--- Ejercicio1/Servidor/Program.cs
+++ Ejercicio1/Servidor/Program.cs
@@ -7,17 +7,19 @@ using System.Threading;
 using NetworkStreamNS;
 using CarreteraClass;
 using VehiculoClass;
+using ClienteClass;
 
 namespace Servidor
 {
 
     class Program
     {
-
+        static TcpListener servidor;
+        
         static void Main(string[] args)
-        {            
-
-
+        {
+            // Crear el servidor TCP            
+            servidor = new TcpListener(IPAddress.Parse("127.0.0.1"), 10001);
         }
     }
 }
