fce4611f66051c23eb07410af5bdd987f5edb01e
liabafi <lbatllefigueras@gmail.com>
2025-04-23 20:28:58 +0200

    Código servidor completado.

--- Ejercicio1/Servidor/Program.cs
+++ Ejercicio1/Servidor/Program.cs
@@ -5,9 +5,7 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using NetworkStreamNS;
-using CarreteraClass;
 using VehiculoClass;
-using ClienteClass;
 
 namespace Servidor
 {
@@ -15,13 +13,82 @@ namespace Servidor
     class Program
     {
         static TcpListener servidor;
+        static readonly object locker = new object();
+        static int id = 0;
+        static ClienteNuevo clienteNuevo = new ClienteNuevo();
+        static List<ClienteNuevo> clientesConectados = new List<ClienteNuevo>();
         
         static void Main(string[] args)
         {
-            // Crear el servidor TCP            
             servidor = new TcpListener(IPAddress.Parse("127.0.0.1"), 10001);
             servidor.Start();
-            Console.WriteLine("Servidor iniciado.");
+            Console.WriteLine("Servidor: Servidor iniciado");
+
+            while (true)
+            {
+                TcpClient cliente = servidor.AcceptTcpClient();
+                Thread clienteThread = new Thread(GestionarCliente);
+                clienteThread.Start(cliente);
+            }
+        }
+
+        private static void GestionarCliente(object clienteObj)
+        {
+            byte[] bufferLectura = new byte[1024];
+            TcpClient cliente = (TcpClient)clienteObj;
+
+            if (cliente.Connected)
+            {
+                int idCliente;
+
+                lock (locker)
+                {
+                    id++;
+                    Console.WriteLine("Gestionando cliente {0}...", id);
+                    idCliente = id;
+                }
+
+                NetworkStream NS = cliente.GetStream();
+                ClienteNuevo clienteNuevo = new ClienteNuevo(idCliente, NS);
+
+                clientesConectados.Add(clienteNuevo);
+                Console.WriteLine("Clientes conectados: " + clientesConectados.Count());
+
+                try 
+                {
+                    bool quit = false;
+                    int mensajeInt;
+                    do
+                    {
+                        string mensaje = NetworkStreamClass.LeerMensajeNetworkStream(NS);
+
+                        if (mensaje == "inicio")
+                        {
+                            mensajeInt = clienteNuevo.id;
+                            mensaje = mensajeInt.ToString();
+
+                            NetworkStreamClass.EscribirMensajeNetworkStream(NS, mensaje);
+                        }
+                        else if (mensaje == "quit")
+                        {
+                            quit = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cliente: Soy el vehiculo ID = " + mensaje);
+                        }
+                    } while (!quit);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error al gestionar al cliente: {0}", e.Message);
+                }
+                finally
+                {
+                    NS.Close();
+                    cliente.Close();
+                }
+            }
         }
     }
 }
