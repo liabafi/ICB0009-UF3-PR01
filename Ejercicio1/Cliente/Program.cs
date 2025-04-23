f32e92f485edaae390954716a14f4646a69cd856
liabafi <lbatllefigueras@gmail.com>
2025-04-23 20:26:09 +0200

    Código Cliente completado.

--- Ejercicio1/Cliente/Program.cs
+++ Ejercicio1/Cliente/Program.cs
@@ -13,17 +13,43 @@ namespace Client
     class Program
     {
         static TcpClient cliente;
+        static NetworkStream NS;
+        static bool quit = false;
+        
 
         static void Main(string[] args)
         {
-            // Crear el cliente TCP
             cliente = new TcpClient();
 
-            cliente.Connect("127.0.0.1", 10001);
-            if (cliente.Connected) {
-                Console.WriteLine("Cliente: Conectado al servidor.");
+            try
+            {
+                cliente.Connect("127.0.0.1", 10001);
+                if (cliente.Connected)
+                {
+                    Console.WriteLine("Cliente: Conectado al servidor.");
+
+                    NS = cliente.GetStream();
+                    string mensaje = "";
+
+                    do
+                    {
+                        if (NS.CanWrite)
+                        {
+                            mensaje = Console.ReadLine();
+                            NetworkStreamClass.EscribirMensajeNetworkStream(NS, mensaje);
+                        }
+                    } while (mensaje != "quit");
+                }
+            }
+            catch (Exception e )
+            {
+                Console.WriteLine("Error al conectar con el servidor: {0}", e.Message);
+            }
+            finally
+            {
+                NS.Close();
+                cliente.Close();
             }
         }
-
     }
 }
